d3a5da1f3aa2004366ebc52c550c328b6f3ef17e
Andrew Mcconville <andrew@andrewmcconville.com>
2023-11-12 11:48:30 -0600

    adds green and refactor for multi color and HSL

--- sketch.js
+++ sketch.js
@@ -1,45 +1,26 @@
-let cameraWidth;
+let cameraWidth = 480;
+let cameraHeight = 480;
 let captureScale = 1;
 let capture;
 let captureConstraints;
-let captureHD;
-let captureHDConstraints;
 
-function setup() {
-  cameraWidth = 480;
-  let canvas = createCanvas(480, 480);
-  canvas.elt.getContext('2d', { willReadFrequently: true });
-  //createCanvas(480, 480);
+function setup() {  
+  pixelDensity(1);
+  createCanvas(cameraWidth, cameraHeight);
   frameRate(60);
   
-  // captureConstraints = {
-  //   video: {
-  //     facingMode: "environment",
-  //     frameRate: { ideal: 10  },
-  //     width: { ideal: 480/captureScale },
-  //     height: { ideal: 480/captureScale }
-  //   },
-  //   audio: false
-  // };
-  // capture = createCapture(captureConstraints, function(stream) {});
-  // capture.size(480/captureScale, 480/captureScale);
-  // capture.hide();
-  
-  captureHDConstraints = {
+  captureConstraints = {
     video: {
       facingMode: "environment",
       frameRate: { ideal: 60  },
-      width: { ideal: 480 },
-      height: { ideal: 480 }
+      width: { ideal: cameraWidth },
+      height: { ideal: cameraHeight }
     },
     audio: false
   };
-  captureHD = createCapture(captureHDConstraints, function(stream) {});
-  captureHD.size(480, 480);
-  //captureHD.hide();
-  capture = captureHD;
-  
-  pixelDensity(1);
+  capture = createCapture(captureConstraints, function(stream) {});
+  capture.size(cameraWidth, cameraHeight);
+  capture.hide();
 }
 
 function draw() {
@@ -47,54 +28,65 @@ function draw() {
 
   capture.loadPixels();
 
-  let clusters = [];
-  let visited = new Set();
+  let redClusters = findClusters(capture.pixels, capture.width, capture.height, isRed);
+  let greenClusters = findClusters(capture.pixels, capture.width, capture.height, isGreen);
 
-  for (let y = 0; y < capture.height; y++) {
-    for (let x = 0; x < capture.width; x++) {
-      let index = (x + y * capture.width) * 4;
-      if (isRed(capture.pixels, index) && !visited.has(index)) {
-        let cluster = getCluster(capture.pixels, x, y, capture.width, visited);
-        if (cluster.length > 0) {
-          clusters.push(cluster);
-        }
-      }
-    }
+  image(capture, 0, 0, cameraWidth, cameraHeight);
+
+  let largestRedCluster = findLargestCluster(redClusters, isRed);
+  let largestGreenCluster = findLargestCluster(greenClusters, isGreen);
+
+  if (largestRedCluster) {
+    let redBoundingBox = boundingBox(largestRedCluster);
+    drawBoundingBox(redBoundingBox, [0, 255, 0]);
   }
-
-  image(captureHD, 0, 0, 480, 480);
-
-  let largestCluster = findLargestCluster(clusters);
-
-  if (largestCluster) {
-    noFill();
-    stroke(0, 255, 0);
-    strokeWeight(2);
-    rectMode(CORNERS);
-    let [minX, minY, maxX, maxY] = boundingBox(largestCluster).rect;
-    rect(minX, minY, maxX, maxY);
-    fill(0, 255, 0);
-    noStroke();
-    ellipse(boundingBox(largestCluster).center.x, boundingBox(largestCluster).center.y, 6, 6);
+  
+  if (largestGreenCluster) {
+    let greenBoundingBox = boundingBox(largestGreenCluster);
+    drawBoundingBox(greenBoundingBox, [0, 255, 0]);
   }
+}
+
+function drawBoundingBox(box, color) {
+  noFill();
+  stroke(...color);
+  strokeWeight(2);
+  rectMode(CORNERS);
+  rect(box.rect[0], box.rect[1], box.rect[2], box.rect[3]);
+  fill(...color);
+  noStroke();
+  ellipse(box.center.x, box.center.y, 6, 6);
 }
 
 function isRed(pixels, index) {
-  let isRed = false;
-  let threshold = 70;
+  let r = pixels[index];
+  let g = pixels[index + 1];
+  let b = pixels[index + 2];
 
-  if (
-    pixels[index] > 150 &&
-    pixels[index] - pixels[index + 1] > threshold &&
-    pixels[index] - pixels[index + 2] > threshold
-    ) {
-    isRed = true;
-  }
+  let [h, s, l] = rgbToHsl(r, g, b);
 
-  return isRed;
+  let isRedHue = (h > 0 && h < 10) || (h > 340 && h < 360);
+  let isSaturated = s > 40;
+  let isNotTooLight = l > 30 && l < 80;
+
+  return isRedHue && isSaturated && isNotTooLight;
 }
 
-function getCluster(pixels, startX, startY, width, visited) {
+function isGreen(pixels, index) {
+  let r = pixels[index];
+  let g = pixels[index + 1];
+  let b = pixels[index + 2];
+
+  let [h, s, l] = rgbToHsl(r, g, b);
+
+  let isGreenHue = h > 90 && h < 140;
+  let isSaturated = s > 40;
+  let isNotTooLight = l > 30 && l < 80;
+
+  return isGreenHue && isSaturated && isNotTooLight;
+}
+
+function getCluster(pixels, startX, startY, width, visited, isColor) {
   let cluster = [];
   let stack = [[startX, startY]];
 
@@ -103,7 +95,7 @@ function getCluster(pixels, startX, startY, width, visited) {
     let index = (x + y * width) * 4;
 
     if (x >= 0 && y >= 0 && x < width && y < pixels.length / (width * 4) &&
-        isRed(pixels, index) && !visited.has(index)) {
+      isColor(pixels, index) && !visited.has(index)) {
       visited.add(index);
       cluster.push([x, y]);
       stack.push([x + 1, y], [x - 1, y], [x, y + 1], [x, y - 1]);
@@ -111,6 +103,25 @@ function getCluster(pixels, startX, startY, width, visited) {
   }
 
   return cluster;
+}
+
+function findClusters(pixels, width, height, isColor) {
+  let clusters = [];
+  let visited = new Set();
+
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+      let index = (x + y * width) * 4;
+      if (isColor(pixels, index) && !visited.has(index)) {
+        let cluster = getCluster(pixels, x, y, width, visited, isColor);
+        if (cluster.length > 0) {
+          clusters.push(cluster);
+        }
+      }
+    }
+  }
+
+  return clusters;
 }
 
 function findLargestCluster(clusters) {
@@ -133,4 +144,28 @@ function boundingBox(cluster) {
     rect: [minX*captureScale, minY*captureScale, maxX*captureScale, maxY*captureScale],
     center: createVector(centerX*captureScale, centerY*captureScale)
   };
+}
+
+function rgbToHsl(r, g, b) {
+  r /= 255, g /= 255, b /= 255;
+  let max = Math.max(r, g, b), min = Math.min(r, g, b);
+  let h, s, l = (max + min) / 2;
+
+  if(max == min){
+    h = s = 0; // achromatic
+  } else {
+    let d = max - min;
+    s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+    switch(max){
+      case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+      case g: h = (b - r) / d + 2; break;
+      case b: h = (r - g) / d + 4; break;
+    }
+    h *= 60; // Convert from 0-6 range to 0-360 range for hue
+  }
+
+  s *= 100; // Convert from 0-1 range to 0-100 range for saturation
+  l *= 100; // Convert from 0-1 range to 0-100 range for lightness
+
+  return [h, s, l];
 }
