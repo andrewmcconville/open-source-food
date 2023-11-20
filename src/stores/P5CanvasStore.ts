07813978f8df4d8d998acb21a402235ac7b46831
Andrew Mcconville <andrew@andrewmcconville.com>
2023-11-20 08:44:06 -0600

    moving some p5canvas settings to a store

---
+++ src/stores/P5CanvasStore.ts
@@ -0,0 +1,24 @@
+import { defineStore } from 'pinia';
+
+export const useP5CanvasStore = defineStore('P5CanvasStore', {
+  state: () => ({
+    isLooping: false as boolean,
+    throttleClusterSearch: 4 as number,
+  }),
+  getters: {
+    getIsLooping(): boolean {
+        return this.isLooping;
+    },
+    getThrottleClusterSearch(): number {
+        return this.throttleClusterSearch;
+    },
+  },
+  actions: {
+    setIsLooping(isLooping: boolean) {
+      this.isLooping = isLooping;
+    },
+    setThrottleClusterSearch(throttleClusterSearch: number) {
+      this.throttleClusterSearch = throttleClusterSearch;
+    },
+  },
+});
