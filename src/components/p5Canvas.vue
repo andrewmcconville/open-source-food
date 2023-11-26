ed94777b4f974128ad450daf040a6a285396f880
Andrew Mcconville <andrew@andrewmcconville.com>
2023-11-26 10:54:53 -0600

    adds meat and bread

--- src/components/p5Canvas.vue
+++ src/components/p5Canvas.vue
@@ -25,15 +25,29 @@ onMounted(() => {
         let capture: any;
         let captureConstraints;
         let frameCount: number = 0;
-        let redClusters = [];
-        let greenClusters = [];
-        let largestRedCluster: Array<[number, number]>;
-        let largestGreenCluster: Array<[number, number]>;
-        let redBoundingBox: P5BoundingBox;
-        let oldRedBoundingBox: P5BoundingBox;
-        let greenBoundingBox: P5BoundingBox;
-        let oldGreenBoundingBox: P5BoundingBox;
         let lerpAmount: number = 1;
+
+        //TO DO: these four things are not DRY
+        let tomatoClusters = [];
+        let largestTomatoCluster: Array<[number, number]>;
+        let tomatoBoundingBox: P5BoundingBox;
+        let oldTomatoBoundingBox: P5BoundingBox;
+
+        let lettuceClusters = [];
+        let largestLettuceCluster: Array<[number, number]>;
+        let lettuceBoundingBox: P5BoundingBox;
+        let oldLettuceBoundingBox: P5BoundingBox;
+
+        let breadClusters = [];
+        let largeestBreadCluster: Array<[number, number]>;
+        let breadBoundingBox: P5BoundingBox;
+        let oldBreadBoundingBox: P5BoundingBox;
+
+        let meatClusters = [];
+        let largeestMeatCluster: Array<[number, number]>;
+        let meatBoundingBox: P5BoundingBox;
+        let oldMeatBoundingBox: P5BoundingBox;
+
 
         p.setup = () => {
             p5CanvasStore.setIsLooping(true);
@@ -64,41 +78,73 @@ onMounted(() => {
             if (frameCount % p5CanvasStore.getThrottleClusterSearch === 0) {
                 capture.loadPixels();
                 updateClusters();
-                if (largestRedCluster) {
-                    oldRedBoundingBox = redBoundingBox;
-                    redBoundingBox = getBoundingBox(largestRedCluster);
+                if (largestTomatoCluster) {
+                    oldTomatoBoundingBox = tomatoBoundingBox;
+                    tomatoBoundingBox = getBoundingBox(largestTomatoCluster);
                 }
-                if (largestGreenCluster) {
-                    oldGreenBoundingBox = greenBoundingBox;
-                    greenBoundingBox = getBoundingBox(largestGreenCluster);
+                if (largestLettuceCluster) {
+                    oldLettuceBoundingBox = lettuceBoundingBox;
+                    lettuceBoundingBox = getBoundingBox(largestLettuceCluster);
+                }
+                if (largeestBreadCluster) {
+                    oldBreadBoundingBox = breadBoundingBox;
+                    breadBoundingBox = getBoundingBox(largeestBreadCluster);
+                }
+                if (largeestMeatCluster) {
+                    oldMeatBoundingBox = meatBoundingBox;
+                    meatBoundingBox = getBoundingBox(largeestMeatCluster);
                 }
                 frameCount = 0;
             }
             frameCount++;            
             lerpAmount = frameCount / p5CanvasStore.getThrottleClusterSearch;
 
-            if (largestRedCluster && oldRedBoundingBox) {
+            if (largestTomatoCluster && oldTomatoBoundingBox) {
                 let lerpBox: P5BoundingBox = {rect: [0, 0, 0, 0], center: p.createVector(0, 0)};
 
-                lerpBox.rect[0] = p.lerp(oldRedBoundingBox.rect[0], redBoundingBox.rect[0], lerpAmount);
-                lerpBox.rect[1] = p.lerp(oldRedBoundingBox.rect[1], redBoundingBox.rect[1], lerpAmount);
-                lerpBox.rect[2] = p.lerp(oldRedBoundingBox.rect[2], redBoundingBox.rect[2], lerpAmount);
-                lerpBox.rect[3] = p.lerp(oldRedBoundingBox.rect[3], redBoundingBox.rect[3], lerpAmount);
-                lerpBox.center = p5.Vector.lerp(oldRedBoundingBox.center, redBoundingBox.center, lerpAmount);
+                lerpBox.rect[0] = p.lerp(oldTomatoBoundingBox.rect[0], tomatoBoundingBox.rect[0], lerpAmount);
+                lerpBox.rect[1] = p.lerp(oldTomatoBoundingBox.rect[1], tomatoBoundingBox.rect[1], lerpAmount);
+                lerpBox.rect[2] = p.lerp(oldTomatoBoundingBox.rect[2], tomatoBoundingBox.rect[2], lerpAmount);
+                lerpBox.rect[3] = p.lerp(oldTomatoBoundingBox.rect[3], tomatoBoundingBox.rect[3], lerpAmount);
+                lerpBox.center = p5.Vector.lerp(oldTomatoBoundingBox.center, tomatoBoundingBox.center, lerpAmount);
 
                 drawBoundingBox(lerpBox, p.color(0, 255, 0));
             };
 
-            if (largestGreenCluster && oldGreenBoundingBox) {
+            if (largestLettuceCluster && oldLettuceBoundingBox) {
                 let lerpBox: P5BoundingBox = {rect: [0, 0, 0, 0], center: p.createVector(0, 0)};
 
-                lerpBox.rect[0] = p.lerp(oldGreenBoundingBox.rect[0], greenBoundingBox.rect[0], lerpAmount);
-                lerpBox.rect[1] = p.lerp(oldGreenBoundingBox.rect[1], greenBoundingBox.rect[1], lerpAmount);
-                lerpBox.rect[2] = p.lerp(oldGreenBoundingBox.rect[2], greenBoundingBox.rect[2], lerpAmount);
-                lerpBox.rect[3] = p.lerp(oldGreenBoundingBox.rect[3], greenBoundingBox.rect[3], lerpAmount);
-                lerpBox.center = p5.Vector.lerp(oldGreenBoundingBox.center, greenBoundingBox.center, lerpAmount);
+                lerpBox.rect[0] = p.lerp(oldLettuceBoundingBox.rect[0], lettuceBoundingBox.rect[0], lerpAmount);
+                lerpBox.rect[1] = p.lerp(oldLettuceBoundingBox.rect[1], lettuceBoundingBox.rect[1], lerpAmount);
+                lerpBox.rect[2] = p.lerp(oldLettuceBoundingBox.rect[2], lettuceBoundingBox.rect[2], lerpAmount);
+                lerpBox.rect[3] = p.lerp(oldLettuceBoundingBox.rect[3], lettuceBoundingBox.rect[3], lerpAmount);
+                lerpBox.center = p5.Vector.lerp(oldLettuceBoundingBox.center, lettuceBoundingBox.center, lerpAmount);
 
                 drawBoundingBox(lerpBox, p.color(255, 0, 0));
+            }
+
+            if (largeestBreadCluster && oldBreadBoundingBox) {
+                let lerpBox: P5BoundingBox = {rect: [0, 0, 0, 0], center: p.createVector(0, 0)};
+
+                lerpBox.rect[0] = p.lerp(oldBreadBoundingBox.rect[0], breadBoundingBox.rect[0], lerpAmount);
+                lerpBox.rect[1] = p.lerp(oldBreadBoundingBox.rect[1], breadBoundingBox.rect[1], lerpAmount);
+                lerpBox.rect[2] = p.lerp(oldBreadBoundingBox.rect[2], breadBoundingBox.rect[2], lerpAmount);
+                lerpBox.rect[3] = p.lerp(oldBreadBoundingBox.rect[3], breadBoundingBox.rect[3], lerpAmount);
+                lerpBox.center = p5.Vector.lerp(oldBreadBoundingBox.center, breadBoundingBox.center, lerpAmount);
+
+                drawBoundingBox(lerpBox, p.color(0, 0, 255));
+            }
+
+            if (largeestMeatCluster && oldMeatBoundingBox) {
+                let lerpBox: P5BoundingBox = {rect: [0, 0, 0, 0], center: p.createVector(0, 0)};
+
+                lerpBox.rect[0] = p.lerp(oldMeatBoundingBox.rect[0], meatBoundingBox.rect[0], lerpAmount);
+                lerpBox.rect[1] = p.lerp(oldMeatBoundingBox.rect[1], meatBoundingBox.rect[1], lerpAmount);
+                lerpBox.rect[2] = p.lerp(oldMeatBoundingBox.rect[2], meatBoundingBox.rect[2], lerpAmount);
+                lerpBox.rect[3] = p.lerp(oldMeatBoundingBox.rect[3], meatBoundingBox.rect[3], lerpAmount);
+                lerpBox.center = p5.Vector.lerp(oldMeatBoundingBox.center, meatBoundingBox.center, lerpAmount);
+
+                drawBoundingBox(lerpBox, p.color(255, 255, 255));
             }
         }
 
@@ -116,7 +162,7 @@ onMounted(() => {
 
         function navigate(event: MouseEvent | TouchEvent) {
             if (event.target === p.canvas) {
-                let clickedBox: string | null = getClickedBox(p.mouseX, p.mouseY, redBoundingBox, greenBoundingBox);
+                let clickedBox: string | null = getClickedBox(p.mouseX, p.mouseY, tomatoBoundingBox, lettuceBoundingBox, breadBoundingBox, meatBoundingBox);
                 if (clickedBox) {
                     router.push({ name: 'IngredientDetails', params: { id: clickedBox } });
                 } else {
@@ -125,12 +171,18 @@ onMounted(() => {
             }
         }
 
-        function getClickedBox(mouseX: number, mouseY: number, redBoundingBox: P5BoundingBox, greenBoundingBox: P5BoundingBox): string {
-            if (isMouseInsideBox(mouseX, mouseY, redBoundingBox)) {
+        function getClickedBox(mouseX: number, mouseY: number, tomatoBoundingBox: P5BoundingBox, lettuceBoundingBox: P5BoundingBox, breadBoundingBox: P5BoundingBox, meatBoundingBox: P5BoundingBox): string {
+            if (isMouseInsideBox(mouseX, mouseY, tomatoBoundingBox)) {
                 return "tomato";
             }
-            else if (isMouseInsideBox(mouseX, mouseY, greenBoundingBox)) {
+            else if (isMouseInsideBox(mouseX, mouseY, lettuceBoundingBox)) {
                 return "lettuce";
+            }
+            else if (isMouseInsideBox(mouseX, mouseY, breadBoundingBox)) {
+                return "bread";
+            }
+            else if (isMouseInsideBox(mouseX, mouseY, meatBoundingBox)) {
+                return "meat";
             }
             else {
                 return '';
@@ -173,21 +225,21 @@ onMounted(() => {
             };
         }
 
-        function isRed(pixels, index): boolean {
+        function isTomato(pixels, index): boolean {
             let red: number = pixels[index];
             let green: number = pixels[index + 1];
             let blue: number = pixels[index + 2];
 
             let [h, s, b] = rgbToHsb(red, green, blue);
 
-            let withinHue = (h > 0 && 15 > h) || (h > 350 && 360 > h);
+            let withinHue = (h > 0 && 30 > h) || (h > 350 && 360 > h);
             let withinSaturation = s > 40 && 100 > s;
             let withinBrightness = b > 40 && 90 > b;
 
             return withinHue && withinSaturation && withinBrightness;
         }
 
-        function isGreen(pixels, index): boolean {
+        function isLettuce(pixels, index): boolean {
             let red: number = pixels[index];
             let green: number = pixels[index + 1];
             let blue: number = pixels[index + 2];
@@ -201,12 +253,46 @@ onMounted(() => {
             return withinHue && withinSaturation && withinBrightness;
         }
 
-        function updateClusters() {
-            redClusters = findClusters(capture.pixels, capture.width, capture.height, isRed);
-            largestRedCluster = findLargestCluster(redClusters);
+        function isBread(pixels, index): boolean {
+            let red: number = pixels[index];
+            let green: number = pixels[index + 1];
+            let blue: number = pixels[index + 2];
 
-            greenClusters = findClusters(capture.pixels, capture.width, capture.height, isGreen);
-            largestGreenCluster = findLargestCluster(greenClusters);
+            let [h, s, b] = rgbToHsb(red, green, blue);
+
+            let withinHue = h > 30 && 50 > h;
+            let withinSaturation = s > 40 && 80 > s;
+            let withinBrightness = b > 40 && 70 > b;
+
+            return withinHue && withinSaturation && withinBrightness;
+        }
+
+        function isMeat(pixels, index): boolean {
+            let red: number = pixels[index];
+            let green: number = pixels[index + 1];
+            let blue: number = pixels[index + 2];
+
+            let [h, s, b] = rgbToHsb(red, green, blue);
+
+            let withinHue = h > 330 && 360 > h;
+            let withinSaturation = s > 20 && 50 > s;
+            let withinBrightness = b > 0 && 50 > b;
+
+            return withinHue && withinSaturation && withinBrightness;
+        }
+
+        function updateClusters() {
+            tomatoClusters = findClusters(capture.pixels, capture.width, capture.height, isTomato);
+            largestTomatoCluster = findLargestCluster(tomatoClusters);
+
+            lettuceClusters = findClusters(capture.pixels, capture.width, capture.height, isLettuce);
+            largestLettuceCluster = findLargestCluster(lettuceClusters);
+
+            breadClusters = findClusters(capture.pixels, capture.width, capture.height, isBread);
+            largeestBreadCluster = findLargestCluster(breadClusters);
+
+            meatClusters = findClusters(capture.pixels, capture.width, capture.height, isMeat);
+            largeestMeatCluster = findLargestCluster(meatClusters);
         }
 
         function findClusters(pixels, width, height, isColor) {
